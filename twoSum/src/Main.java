c175a3618df9fcde13d8db2aed0a9db43dc4838e
kunal <kunalc3626@gmail.com>
2024-04-26 16:26:17 +0530

    Added problem statements of all previous problems

--- twoSum/src/Main.java
+++ twoSum/src/Main.java
@@ -1,8 +1,19 @@
 import java.util.HashMap;
 import java.util.Arrays;
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+ */
+
 public class Main {
     public static void main(String[] args) {
        int[] nums = new int[]{2,7,11,15};
